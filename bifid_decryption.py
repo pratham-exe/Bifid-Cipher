8e43fd5997e7dca10c74e8d5ac47d67a8a2229a2
Pratham Rao U N <116641297+pratham-exe@users.noreply.github.com>
2023-10-07 19:50:31 +0530

    Add files via upload

---
+++ bifid_decryption.py
@@ -0,0 +1,68 @@
+import bifid
+import bifid_encryption
+sublist = []
+for letter in bifid_encryption.final:
+    for row in range(5):
+        for col in range(5):
+            if letter == 'J':
+                sublist.extend([2,0])
+            elif letter == bifid.table[row][col]:
+                sublist.extend([row,col])
+            else:
+                continue
+size = len(sublist)
+rowlist = []
+collist = []
+def splitting(i):
+    count = 0
+    while i > 0:
+        if i == 2:
+            rowlist.append(sublist[count])
+            count += 1
+            collist.append(sublist[count])
+            i -= 2
+        elif i == 4:
+            for _ in range(2):
+                rowlist.append(sublist[count])
+                count += 1
+            for _ in range(2):
+                collist.append(sublist[count])
+                count += 1
+            i -= 4
+        elif i == 6:
+            for _ in range(3):
+                rowlist.append(sublist[count])
+                count += 1
+            for _ in range(3):
+                collist.append(sublist[count])
+                count += 1
+            i -= 6
+        else:
+            for _ in range(4):
+                rowlist.append(sublist[count])
+                count += 1
+            for _ in range(4):
+                collist.append(sublist[count])
+                count += 1
+            i -= 8
+splitting(size)
+output = []
+output1 = []
+rowsize = len(rowlist)
+for i in range(rowsize):
+    output.append(bifid.table[rowlist[i]][collist[i]])
+for i in range(rowsize):
+    if rowlist[i] == 2 and collist[i] == 0:
+        output1.append('J')
+    else:
+        output1.append(bifid.table[rowlist[i]][collist[i]])
+key_decrypt = input("\nKey please : ")
+if key_decrypt == bifid_encryption.key:
+    print("Decrypted word is : ",end="")
+    for ouput_letter in output:
+        print(ouput_letter,end="")
+    print(" / ",end="")
+    for output1_letter in output1:
+        print(output1_letter,end="")
+else:
+    print("Sorry ! Key not matching .")
--- bifid_encryption.py
+++ bifid_encryption.py
@@ -1,11 +1,6 @@
-import random
-symbols = ['.','?','!',',','/']
-table = [['B','G','W','K','Z'],
-         ['Q','P','N','D','S'],
-         ['I','O','A','X','E'],
-         ['F','C','L','U','M'],
-         ['T','H','Y','V','R']]
+import bifid
 plaintext = input("Enter the word to be encrypted : ").upper()
+key = input("Enter the key to protect this plaintext word : ")
 plainlist = []
 rowlist = []
 columnlist = []
@@ -16,13 +11,10 @@ for letter in plainlist:
     if letter == 'J':
         rowlist.append(2)
         columnlist.append(0)
-    elif letter in symbols:
-        rowlist.append(random.choice(range(5)))
-        columnlist.append(random.choice(range(5)))
     else:
         for row in range(5):
             for column in range(5):
-                if (letter == table[row][column]):
+                if (letter == bifid.table[row][column]):
                     rowlist.append(row)
                     columnlist.append(column)
 row_size = len(rowlist)
@@ -44,19 +36,9 @@ sub_size = len(sublist)
 index = 0
 final = []
 while (index < sub_size):
-    final.append(table[sublist[index]][sublist[index+1]])
+    final.append(bifid.table[sublist[index]][sublist[index+1]])
     index = index + 2
 final_size = len(final)
 print("Encrypted word is : ",end="")
-j = 0
-output = []
-while (j <= final_size):
-    for final_row in range(4):
-        if final_row+j < final_size:
-           output.append(final[final_row+j]) 
-        else:
-            continue
-    output.append(" ")
-    j = j + 4
-for final_letter in output:
+for final_letter in final:
     print(final_letter,end="")
